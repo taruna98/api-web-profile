32e54ae4fa8d88714e7737c4868232cd78b5c658
unknown <jhonny.ocnr@gmail.com>
2023-10-27 23:38:37 +0700

    additional conditional navbar and json for article

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,27 +20,47 @@ class ProfileController extends Controller
             ->first();
 
         // get data portfolio from json file
-        $folderPath = realpath(__DIR__ . '/../../../') . '\public\json\portfolio';
+        $folderPathPor = realpath(__DIR__ . '/../../../') . '\public\json\portfolio';
 
-        // get all file JSON in directory
-        $jsonFiles = glob($folderPath . '/*.json');
+        // get all file JSON in directory portfolio
+        $jsonFilesPor = glob($folderPathPor . '/*.json');
 
-        foreach ($jsonFiles as $jsonFile) {
-            $jsonData = file_get_contents($jsonFile);
-            $decodedData = json_decode($jsonData, true);
-            if (strpos($decodedData['id'], $profile->cod) !== false) {
-                $data['id']     = $decodedData['id'];
-                $data['ttl']    = $decodedData['ttl'];
-                $data['hbg']    = $decodedData['hbg'];
-                $data['ctg']    = $decodedData['ctg'];
-                $data['cat']    = $decodedData['cat'];
-                $portfolio[]    = $data;
+        foreach ($jsonFilesPor as $jsonFilePor) {
+            $jsonDataPor = file_get_contents($jsonFilePor);
+            $decodedDataPor = json_decode($jsonDataPor, true);
+            if (strpos($decodedDataPor['id'], $profile->cod) !== false) {
+                $dataPor['id']  = $decodedDataPor['id'];
+                $dataPor['ttl'] = $decodedDataPor['ttl'];
+                $dataPor['hbg'] = $decodedDataPor['hbg'];
+                $dataPor['ctg'] = $decodedDataPor['ctg'];
+                $dataPor['cat'] = $decodedDataPor['cat'];
+                $portfolio[]    = $dataPor;
+            }
+        }
+
+        // get data article from json file
+        $folderPathArt = realpath(__DIR__ . '/../../../') . '\public\json\article';
+
+        // get all file JSON in directory article
+        $jsonFilesArt = glob($folderPathArt . '/*.json');
+
+        foreach ($jsonFilesArt as $jsonFileArt) {
+            $jsonDataArt = file_get_contents($jsonFileArt);
+            $decodedDataArt = json_decode($jsonDataArt, true);
+            if (strpos($decodedDataArt['id'], $profile->cod) !== false) {
+                $dataArt['id']  = $decodedDataArt['id'];
+                $dataArt['nme'] = $decodedDataArt['nme'];
+                $dataArt['ttl'] = $decodedDataArt['ttl'];
+                $dataArt['ctg'] = $decodedDataArt['ctg'];
+                $dataArt['cat'] = $decodedDataArt['cat'];
+                $article[]      = $dataArt;
             }
         }
 
         $dataload = [
             'profile'   => $profile,
-            'portfolio' => $portfolio
+            'portfolio' => $portfolio,
+            'article'   => $article
         ];
         
         return response($dataload, 200);
