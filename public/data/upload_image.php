1e01076a73769073922131575a6cda0b1a16549e
taruna98 <taruna.ersa@gmail.com>
2024-04-15 19:38:39 +0700

    fixing logical to upload multiple image for portfolio kretech

--- public/data/upload_image.php
+++ public/data/upload_image.php
@@ -6,87 +6,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $tempPath = $_FILES['portfolio_file_1']['tmp_name']; // path temporary file upload
         $destinationPath = $uploadDirectory . $_FILES['portfolio_file_1']['name']; // path file destination
         // move file uploaded to destination directory
-        if (move_uploaded_file($tempPath, $destinationPath)) {
-            // echo 'success';
-            // die();
-        } else {
+        if (!move_uploaded_file($tempPath, $destinationPath)) {
             echo 'failed';
             die();
         }
-    } else {
-        echo 'failed';
-        die();
-    }
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['portfolio_file_2']) && $_FILES['portfolio_file_2']['error'] === UPLOAD_ERR_OK) {
+    } else if (isset($_FILES['portfolio_file_2']) && $_FILES['portfolio_file_2']['error'] === UPLOAD_ERR_OK) {
         $tempPath = $_FILES['portfolio_file_2']['tmp_name']; // path temporary file upload
         $destinationPath = $uploadDirectory . $_FILES['portfolio_file_2']['name']; // path file destination
         // move file uploaded to destination directory
-        if (move_uploaded_file($tempPath, $destinationPath)) {
-            // echo 'success';
-            // die();
-        } else {
+        if (!move_uploaded_file($tempPath, $destinationPath)) {
             echo 'failed';
             die();
         }
-    } else {
-        echo 'failed';
-        die();
-    }
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['portfolio_file_3']) && $_FILES['portfolio_file_3']['error'] === UPLOAD_ERR_OK) {
+    } else if (isset($_FILES['portfolio_file_3']) && $_FILES['portfolio_file_3']['error'] === UPLOAD_ERR_OK) {
         $tempPath = $_FILES['portfolio_file_3']['tmp_name']; // path temporary file upload
         $destinationPath = $uploadDirectory . $_FILES['portfolio_file_3']['name']; // path file destination
         // move file uploaded to destination directory
-        if (move_uploaded_file($tempPath, $destinationPath)) {
-            // echo 'success';
-            // die();
-        } else {
+        if (!move_uploaded_file($tempPath, $destinationPath)) {
             echo 'failed';
             die();
         }
-    } else {
-        echo 'failed';
-        die();
-    }
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['portfolio_file_4']) && $_FILES['portfolio_file_4']['error'] === UPLOAD_ERR_OK) {
+    } else if (isset($_FILES['portfolio_file_4']) && $_FILES['portfolio_file_4']['error'] === UPLOAD_ERR_OK) {
         $tempPath = $_FILES['portfolio_file_4']['tmp_name']; // path temporary file upload
         $destinationPath = $uploadDirectory . $_FILES['portfolio_file_4']['name']; // path file destination
         // move file uploaded to destination directory
-        if (move_uploaded_file($tempPath, $destinationPath)) {
-            // echo 'success';
-            // die();
-        } else {
+        if (!move_uploaded_file($tempPath, $destinationPath)) {
             echo 'failed';
             die();
         }
-    } else {
-        echo 'failed';
-        die();
-    }
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['portfolio_file_5']) && $_FILES['portfolio_file_5']['error'] === UPLOAD_ERR_OK) {
+    } else if (isset($_FILES['portfolio_file_5']) && $_FILES['portfolio_file_5']['error'] === UPLOAD_ERR_OK) {
         $tempPath = $_FILES['portfolio_file_5']['tmp_name']; // path temporary file upload
         $destinationPath = $uploadDirectory . $_FILES['portfolio_file_5']['name']; // path file destination
         // move file uploaded to destination directory
-        if (move_uploaded_file($tempPath, $destinationPath)) {
-            // echo 'success';
-            // die();
-        } else {
+        if (!move_uploaded_file($tempPath, $destinationPath)) {
             echo 'failed';
             die();
         }
-    } else {
-        echo 'failed';
-        die();
     }
 }
